0e75ec90d245c11d8244ce596553495f24c77bd0
Francesco Abbate <francesco.bbt@gmail.com>
2020-06-12 11:53:56 +0200

    Add Monokai color theme for testing purpose

---
+++ data/user/colors/monokai.lua
@@ -0,0 +1,29 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#282923" }
+style.background2 = { common.color "#3E3D32" }
+style.background3 = { common.color "#3E3D32" }
+style.text = { common.color "#97979C" }
+style.caret = { common.color "#93DDFA" }
+style.accent = { common.color "#e1e1e6" }
+style.dim = { common.color "#525257" }
+style.divider = { common.color "#202024" }
+style.selection = { common.color "#48484f" }
+style.line_number = { common.color "#525259" }
+style.line_number2 = { common.color "#83838f" }
+style.line_highlight = { common.color "#343438" }
+style.scrollbar = { common.color "#414146" }
+style.scrollbar2 = { common.color "#4b4b52" }
+
+style.syntax["normal"] = { common.color "#F8F8F2" } -- white
+style.syntax["symbol"] = { common.color "#F8F8F2" } -- white
+style.syntax["comment"] = { common.color "#74705D" } -- gray
+style.syntax["keyword"] = { common.color "#F92472" } -- pink
+style.syntax["keyword2"] = { common.color "#67D8EF" } -- cyan
+style.syntax["number"] = { common.color "#AC80FF" } -- violet
+style.syntax["literal"] = { common.color "#AC80FF" } -- violet
+style.syntax["string"] = { common.color "#E7DB74" } -- yellow
+style.syntax["operator"] = { common.color "#F92472" } -- pink
+style.syntax["function"] = { common.color "#A6E22C" } -- green
+
--- data/user/init.lua
+++ data/user/init.lua
@@ -6,7 +6,7 @@ local config = require "core.config"
 local style = require "core.style"
 
 -- light theme:
--- require "user.colors.summer"
+require "user.colors.monokai"
 
 -- key binding:
 -- keymap.add { ["ctrl+escape"] = "core:quit" }
