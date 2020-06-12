3f3334b890c6ae0773b8f0a770c1f18efe6fe603
Francesco Abbate <francesco.bbt@gmail.com>
2020-06-12 14:58:00 +0200

    Add Mariana color scheme for testing

---
+++ data/user/colors/mariana.lua
@@ -0,0 +1,42 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#343D46" } -- background
+style.background2 = { common.color "#343D46" }
+style.background3 = { common.color "#343D46" }
+style.text = { common.color "#97979C" }
+style.caret = { common.color "#93DDFA" }
+style.accent = { common.color "#e1e1e6" }
+style.dim = { common.color "#525257" }
+style.divider = { common.color "#202024" }
+style.selection = { common.color "#48484f" }
+style.line_number = { common.color "#525259" }
+style.line_number2 = { common.color "#83838f" }
+style.line_highlight = { common.color "#343438" }
+style.scrollbar = { common.color "#414146" }
+style.scrollbar2 = { common.color "#4b4b52" }
+
+style.syntax["normal"] = { common.color "#D8DEE9" } -- white
+style.syntax["symbol"] = { common.color "#D8DEE9" } -- white
+style.syntax["comment"] = { common.color "#A6ACB9" } -- gray
+style.syntax["keyword"] = { common.color "#C695C6" } -- violet
+style.syntax["keyword2"] = { common.color "#EC5F66" } -- red
+style.syntax["number"] = { common.color "#F9AE58" } -- yellow
+style.syntax["literal"] = { common.color "#5FB4B4" } -- cyan
+style.syntax["string"] = { common.color "#99C794" } -- green
+style.syntax["operator"] = { common.color "#F97B58" } -- orange
+style.syntax["function"] = { common.color "#6699CC" } -- blue
+
+--[[
+style.syntax["normal"] = { common.color "#D8DEE9" } -- white
+style.syntax["symbol"] = { common.color "#D8DEE9" } -- white
+style.syntax["comment"] = { common.color "#A6ACB9" } -- gray
+style.syntax["keyword"] = { common.color "#C695C6" } -- violet
+style.syntax["keyword2"] = { common.color "#6699CC" } -- blue
+style.syntax["number"] = { common.color "#EC5F66" } -- red
+style.syntax["literal"] = { common.color "#5FB4B4" } -- cyan
+style.syntax["string"] = { common.color "#99C794" } -- green
+style.syntax["operator"] = { common.color "#F97B58" } -- orange
+style.syntax["function"] = { common.color "#F9AE58" } -- yellow
+--]]
+
